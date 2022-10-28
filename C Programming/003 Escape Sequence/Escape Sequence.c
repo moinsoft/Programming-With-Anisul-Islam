545e28c7394fb7fe8c522f5fa3a8a98d83b20b16
moinsoft <mmoin.official@gmail.com>
2022-10-29 02:05:58 +0600

    Using Esacape Sequence

---
+++ C Programming/003 Escape Sequence/Escape Sequence.c
@@ -0,0 +1,15 @@
+/*
+Write a program that prints your name
+Gmail Account
+Phone Number
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("\aMohammad Moin Uddin\n"); // Using Esacape Sequence
+    printf("mmoin.official@gmail.com\n");
+    printf("01571120010");
+    getch();
+}
+
