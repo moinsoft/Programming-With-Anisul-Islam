caa784cd0f7a36f7b850156009c2069a4f3db3f8
moinsoft <mmoin.official@gmail.com>
2022-10-29 01:35:20 +0600

    Comments

---
+++ C Programming/002 Comments/comments.c
@@ -0,0 +1,14 @@
+// This is a program that prints mu name
+
+/*
+Mohammad Moin Uddin
+01571120010
+*/
+
+#include<stdio.h>
+int main()
+{
+    //printf("Mohammad Moin Uddin"); //Printing my name
+    printf("01571120010");
+    getch();
+}
