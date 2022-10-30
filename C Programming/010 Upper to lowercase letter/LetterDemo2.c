eed169c78ad0c665946a515695624395486ac336
moinsoft <mmoin.official@gmail.com>
2022-10-30 23:57:40 +0600

    Upper to lowercase letter

---
+++ C Programming/010 Upper to lowercase letter/LetterDemo2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char upper;
+    printf("Enter any uppercase letter : ");
+    scanf("%c",&upper);
+
+    printf("The lowercase letter : %c",upper+32);
+
+    getch();
+}
+
