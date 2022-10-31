71fd0f52113a70ec28405b3e3f118bcca342acbb
moinsoft <mmoin.official@gmail.com>
2022-10-31 23:45:51 +0600

    Conversion octal to hexa-decima

---
+++ C Programming/017 Conversion octal to hexa-decimal/octalToHexaDecimal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+
+    printf("Octal number = ");
+    scanf("%o",&number);
+
+    printf("Hexa-Decimal number = %x",number);
+
+    getch();
+}
+
+
+
