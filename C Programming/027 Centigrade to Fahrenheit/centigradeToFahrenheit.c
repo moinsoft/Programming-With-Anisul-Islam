f0407d7c0499a247cb91c38a3d6f4fb795e63557
moinsoft <mmoin.official@gmail.com>
2022-11-04 00:25:41 +0600

    Centigrade to Fahrenheit

---
+++ C Programming/027 Centigrade to Fahrenheit/centigradeToFahrenheit.c
@@ -0,0 +1,16 @@
+//Centigrade to Fahrenheit
+
+#include<stdio.h>
+int main()
+{
+    float c,f;
+
+    printf("Enter Centigrade : ");
+    scanf("%f",&c);
+
+    f = (c * 1.8) + 32;
+    printf("Fahrenheit = %.2f\n",f);
+
+
+    getch();
+}
