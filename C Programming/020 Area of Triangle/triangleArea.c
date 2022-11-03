5d6e7082285680020d830c4b25d6d37520af9e67
moinsoft <mmoin.official@gmail.com>
2022-11-03 20:08:37 +0600

    Area of Triangle

---
+++ C Programming/020 Area of Triangle/triangleArea.c
@@ -0,0 +1,22 @@
+//Write a program that calculate the area of a triangle.
+
+#include<stdio.h>
+int main()
+{
+    float base,height,area;
+
+    printf("Base = ");
+    scanf("%f",&base);
+
+    printf("Height = ");
+    scanf("%f",&height);
+
+    area = (float)1/2 * base * height;
+    printf("Area = %.2f",area);
+
+
+
+
+    getch();
+}
+
