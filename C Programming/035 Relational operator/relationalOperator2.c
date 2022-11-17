81c1cfce58bd1df4a3824825077b2666c2f36839
moinsoft <mmoin.official@gmail.com>
2022-11-17 19:23:14 +0600

    Relational Operator Test 2

---
+++ C Programming/035 Relational operator/relationalOperator2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int time = 10;
+
+    if(time==10)
+    {
+        printf("Good Morning\n");
+        printf("Mohammad Moin Uddin\n");
+    }
+    else
+    {
+        printf("Sorry Its Non Morning\n");
+        printf("Mohammad Moin Uddin\n");
+    }
+
+
+    return 0;
+}
