6c4a3ea4e150b377643f75c16a253081f8c08c42
moinsoft <mmoin.official@gmail.com>
2022-11-17 19:35:48 +0600

    Large Number

---
+++ C Programming/036 Large Number/largeNumber.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+
+    printf("Enter First Number : ");
+    scanf("%d",&num1);
+
+    printf("Enter Second Number : ");
+    scanf("%d",&num2);
+
+    if(num1>num2)
+    {
+        printf("Large Number = %d\n",num1);
+    }
+    else if(num1<num2)
+    {
+        printf("Large Number = %d\n",num2);
+    }
+    else
+    {
+        printf("Numbers are equal.\n",num2);
+    }
+
+
+
+    return 0;
+}
+
