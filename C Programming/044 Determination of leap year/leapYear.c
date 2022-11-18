5c15b3fe6e70271adc5ff4458c3bb3dcea214061
moinsoft <mmoin.official@gmail.com>
2022-11-18 19:59:10 +0600

    Determination of leap year

---
+++ C Programming/044 Determination of leap year/leapYear.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+
+    int year;
+
+    printf("Enter any year : ");
+    scanf("%d",&year);
+
+    if(year%400==0)
+    {
+        printf("%d Leap Year\n",year);
+    }
+    else if(year%4==0 && year%100!=0)
+    {
+        printf("%d Leap Year\n",year);
+    }
+    else
+    {
+        printf("%d Not Leap Year.\n",year);
+    }
+}
