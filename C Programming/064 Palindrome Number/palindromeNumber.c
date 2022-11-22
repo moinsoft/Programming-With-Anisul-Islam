90dc8c19943df6a0c53909f002d5752b24d0539d
moinsoft <mmoin.official@gmail.com>
2022-11-22 07:21:26 +0600

    Palindrome Number

---
+++ C Programming/064 Palindrome Number/palindromeNumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num,temp,rem,sum=0;
+
+    printf("Enter any number : ");
+    scanf("%d",&num);
+
+    temp = num;
+
+    while(temp!=0)
+    {
+        rem = temp % 10;
+        sum = sum * 10 + rem;
+        temp = temp / 10;
+    }
+
+    if(num==sum)
+    {
+        printf("Palindrome Number\n");
+    }
+    else
+    {
+        printf("Not Palindrome Number\n");
+    }
+
+    return 0;
+}
