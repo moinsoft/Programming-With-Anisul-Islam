f0fb707f7597493a0973c9d4a1fb4e08d5b3137b
moinsoft <mmoin.official@gmail.com>
2022-11-22 06:45:47 +0600

    Sum of Digits

---
+++ C Programming/062 Sum of Digits/sumOfDigits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,temp,rem,sum=0;
+
+    printf("Enter any number : ");
+    scanf("%d",&num);
+
+    temp = num;
+
+    while(temp!=0)
+    {
+        rem = temp % 10;
+        sum = sum + rem;
+        temp = temp / 10;
+    }
+
+    printf("Sum of Digits : %d\n",sum);
+
+    return 0;
+}
