5ab677232133384d542c5193a5e01266cb387fa7
moinsoft <mmoin.official@gmail.com>
2022-11-26 19:16:11 +0600

    Fibonacci Series

---
+++ C Programming/072 Fibonacci Series/fibonacciSeries.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0,fibo,first=0,second=1;
+
+    printf("Enter Range : ");
+    scanf("%d",&n);
+
+    while(count<n)
+    {
+        if(count<=1)
+        {
+            fibo=count;
+        }
+        else
+        {
+            fibo = first + second;
+            first = second;
+            second = fibo;
+        }
+
+        printf("%d ",fibo);
+        count++;
+
+    }
+
+
+    getch();
+}
