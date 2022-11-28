0687e738804c6e206a692cde319b3b761d70e1a0
moinsoft <mmoin.official@gmail.com>
2022-11-29 01:58:45 +0600

    Pattern 020

---
+++ C Programming/073 Pattern/pattern020.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+
+    printf("Enter n : ");
+    scanf("%d",&n);
+
+    for(row=n; row>=1; row--)
+    {
+        // Printing Space
+        for(col=1; col<=n-row; col++)
+        {
+            printf(" ");
+        }
+
+        // Printing Number
+        for(col=1; col<=row; col++)
+        {
+            printf("%d",col);
+            //printf("%d",row);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
