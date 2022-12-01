5b622c7fb69d885ff269d20a878da82dc95081b0
moinsoft <mmoin.official@gmail.com>
2022-12-01 14:32:04 +0600

    Pattern 035

---
+++ C Programming/073 Pattern/pattern035.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+
+    printf("Enter n : ");
+    scanf("%d",&n);
+
+    for(row=n; row>=1; row--)
+    {
+        // Printing Space
+        for(col=1; col<=n-row; col++)
+        {
+            printf(" ");
+        }
+
+        // Printing Number,Alphabet,Star and Hash
+        for(col=1; col<=row; col++)
+        {
+            printf("%d ",col);
+            //printf("%d ",row);
+            //printf("%d ",col%2);
+            //printf("%d ",row%2);
+            //printf("%c ",col+64);  // Capital Letter
+            //printf("%c ",col+96);  // Small Letter
+            //printf("%c ",row+64);  // Capital Letter
+            //printf("%c ",row+96);  // Small Letter
+            //printf("* ");
+            //printf("# ");
+        }
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
