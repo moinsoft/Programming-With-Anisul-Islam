1ccbdca3afbe1a3fb8adc4fbabff6ce6a8e3ddb2
moinsoft <mmoin.official@gmail.com>
2022-12-03 02:07:50 +0600

    2D Array Printing

---
+++ C Programming/074 Array/007 2D Array Printing/2dArrayPrinting.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int moin[3][4] = { {5,6,7,8}, {9,10,11,12}, {13,14,15,16} };
+
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("%d ",moin[i][j]);
+        }
+
+        printf("\n");
+    }
+
+
+
+    getch();
+}
