bcba8ab041f2e4d4bf47c348974c46064d54d7f5
moinsoft <mmoin.official@gmail.com>
2022-12-07 02:46:02 +0600

    Concatenation without strcat()

---
+++ C Programming/075 String/008 Concatenation without strcat()/concatenationWithoutStrcat.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    char str1[50]="My name is ";
+    char str2[]="Mohammad Moin Uddin.";
+
+    int i=0,len=0,j=0;
+
+    while(str1[i] != '\0')
+    {
+        i++;
+        len++;
+    }
+
+    while(str2[j] != '\0')
+    {
+        str1[len+j] = str2[j] ;
+        j++;
+    }
+
+
+    printf("str1 = %s\n",str1);
+
+
+    getch();
+}
+
