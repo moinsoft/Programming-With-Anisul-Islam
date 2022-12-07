4ccb8a6b00abe73ac6aa983d4d4bcc1be1c03516
moinsoft <mmoin.official@gmail.com>
2022-12-07 10:03:29 +0600

    String reverse using strrev()

---
+++ C Programming/075 String/010 String reverse using strrev()/stringReverseStrrev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[]="Mohammad Moin Uddin";
+
+    printf("String Value = \"%s\"\n",str);
+
+    strrev(str);
+
+    printf("\nString Reverse Value = \"%s\"",str);
+
+
+    getch();
+}
+
