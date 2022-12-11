b2ec398a39d183d90f9708dd36a62a3e41cc48fe
moinsoft <mmoin.official@gmail.com>
2022-12-11 14:21:28 +0600

    Introduction to enum

---
+++ C Programming/078 Union/003 Introduction to enum/enumeration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+enum days_of_week
+{
+    Sun,Mon,Tue,Wed,Thu,Fri,Sat
+};
+
+
+
+int main()
+{
+    enum days_of_week day1,day2;
+
+    day1 = Tue;
+    day2 = Wed;
+
+    int dif = day2-day1;
+
+    printf("Day 1 = %d\n",day1);
+    printf("Day Difference = %d\n",dif);
+
+
+    getch();
+}
+
+
