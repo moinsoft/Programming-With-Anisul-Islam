e5b9ff812537c6bbc062f647d4b1a05ec40b409d
moinsoft <mmoin.official@gmail.com>
2022-12-18 17:45:23 +0600

    How to create header file

---
+++ C Programming/081 Preprocessor/ProjectTest 002/info.h
@@ -0,0 +1,3 @@
+#define name "Moin Uddin"
+#define age 30
+#define fullName printf("Full Name = Mohammad Moin Uddin\n");
---
+++ C Programming/081 Preprocessor/ProjectTest 002/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "info.h"
+
+
+int main()
+{
+    printf("Name = %s\n",name);
+    printf("Age = %d\n",age);
+
+    fullName
+
+
+
+    getch();
+}
