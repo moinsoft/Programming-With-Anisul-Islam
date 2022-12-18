9e6df29dae9ca4e2b4acd7674c643d3d84fd9ebe
moinsoft <mmoin.official@gmail.com>
2022-12-18 16:37:07 +0600

    #define

---
+++ C Programming/081 Preprocessor/ProjectTest 001/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define name "Moin Uddin"
+#define fullName printf("Mohammad Moin Uddin\n");
+
+int main()
+{
+    printf("%s\n",name);
+    fullName
+    fullName
+    fullName
+
+
+
+    getch();
+}
