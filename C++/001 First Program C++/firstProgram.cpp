7bbc86f0cc40684412ce74791b8bd18dffb4ae5b
moinsoft <mmoin.official@gmail.com>
2022-12-19 20:25:15 +0600

    First Program C++

---
+++ C++/001 First Program C++/firstProgram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout<< "Hello World\n";
+    cout<< "Mohammad Moin Uddin\n";
+    cout<< "Mohammad" << " Moin" << " Uddin";
+
+    getch();
+}
