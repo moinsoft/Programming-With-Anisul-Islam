853976da44a1fe3b01f29eb6f018bf00a30a53a6
moinsoft <mmoin.official@gmail.com>
2022-12-22 23:09:29 +0600

    Assignment operator

---
+++ C++/008 Assignment operator/assignmentOperator.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    int y = 2;
+
+    x+=y;    // x = x + y;
+
+    cout << x << endl;
+
+    x-=y;
+
+    cout << x << endl;
+
+    x+=2;
+
+    cout << x << endl;
+
+    x-=2;
+
+    cout << x << endl;
+
+    x*=2;
+
+    cout << x << endl;
+
+    x/=2;
+
+    cout << x << endl;
+
+    x%=2;
+
+    cout << x << endl;
+
+
+    getch();
+}
