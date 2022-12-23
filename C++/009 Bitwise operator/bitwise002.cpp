f58891ffb96168a17ad588ac5365a5750ec092d9
moinsoft <mmoin.official@gmail.com>
2022-12-23 23:30:20 +0600

    Bitwise shift right, shift left operator

---
+++ C++/009 Bitwise operator/bitwise002.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int a = 32;
+    int b = 12;
+    int c;
+
+    c = a >> 3;
+    cout << c << endl;
+
+    c = a << 3;
+    cout << c << endl;
+
+
+    getch();
+}
