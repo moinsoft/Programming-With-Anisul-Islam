983b8f571561683063c8d18aa59b453528994755
moinsoft <mmoin.official@gmail.com>
2022-12-25 01:49:54 +0600

    Vowel-Consonant program using library function

---
+++ C++/011 if - else if - else/008 Vowel-Consonant program using library function/vowelConsonant.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter any letter : ";
+    cin>>ch;
+
+    ch = tolower(ch);
+    //ch = toupper(ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+    {
+        cout<<"Vowel";
+    }
+    else
+    {
+        cout<<"Consonant";
+    }
+
+    getch();
+}
