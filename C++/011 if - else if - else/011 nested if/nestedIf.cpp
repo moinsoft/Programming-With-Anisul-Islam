789a41587299de2309c4e9d45d63655796049f78
moinsoft <mmoin.official@gmail.com>
2022-12-25 02:35:20 +0600

    nested if

---
+++ C++/011 if - else if - else/011 nested if/nestedIf.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int mark;
+
+    cout<<"Enter mark : ";
+    cin>>mark;
+
+    if(mark>32)
+    {
+        if(mark>=80 && mark<=100)
+        {
+            cout<<"A+";
+        }
+        else if(mark>=70 && mark<=79)
+        {
+            cout<<"A";
+        }
+        else if(mark>=60 && mark<=69)
+        {
+            cout<<"A-";
+        }
+        else if(mark>=50 && mark<=59)
+        {
+            cout<<"B";
+        }
+        else if(mark>=40 && mark<=49)
+        {
+            cout<<"C";
+        }
+        else if(mark>=33 && mark<=39)
+        {
+            cout<<"D";
+        }
+    }
+    else
+    {
+        cout<<"Fail"<<endl;
+        cout<<"Best of luck for next time.";
+    }
+
+
+
+    getch();
+}
