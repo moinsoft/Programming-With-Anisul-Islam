67bec7df77eab07640f465f3bfacf7ade9a30606
moinsoft <mmoin.official@gmail.com>
2022-12-25 03:18:34 +0600

    Vowel-consonant program using switch statement

---
+++ C++/013 Switch/002 Vowel-consonant program using switch statement/vowelConsonant.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter any letter : ";
+    cin>>ch;
+
+    ch = tolower(ch);
+
+    switch(ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    //case 'A':
+    //case 'E':
+    //case 'I':
+    //case 'O':
+    //case 'U':
+        cout << "Vowel";
+        break;
+    default:
+        cout << "Consonant";
+    }
+
+    getch();
+}
