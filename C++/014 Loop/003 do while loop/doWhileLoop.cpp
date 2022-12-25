996300807dd252e4d722a833bf45aba1e39c65c6
moinsoft <mmoin.official@gmail.com>
2022-12-25 06:57:42 +0600

    do while loop

---
+++ C++/014 Loop/003 do while loop/doWhileLoop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i=20;
+
+    do{
+
+        cout << "Mohammad Moin Uddin " << i << endl;
+        i--;
+
+    }while(i>=1);
+
+    cout << "End of do while loop";
+
+
+    getch();
+}
