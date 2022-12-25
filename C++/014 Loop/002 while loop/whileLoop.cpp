4022fbaeef48e3d77fd7620a188fdc80685b41d4
moinsoft <mmoin.official@gmail.com>
2022-12-25 06:42:16 +0600

    while loop

---
+++ C++/014 Loop/002 while loop/whileLoop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i=1;
+
+    while(i<=20)
+    {
+        cout << "Bangladesh " << i << endl;
+        i++;
+    }
+
+    cout << "End of while loop";
+
+
+    getch();
+}
